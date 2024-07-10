b907bd82290833139ddcd9484abe5169806df6a0
lmdrn <leamedrano@gmail.com>
2024-07-10 12:50:38 +0200

    change pipe to comma for 2nd multimap

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lmedrano <lmedrano@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 15:44:37 by lmedrano          #+#    #+#             */
-/*   Updated: 2024/07/10 10:42:30 by lmedrano         ###   ########.fr       */
+/*   Updated: 2024/07/10 12:48:42 by lmedrano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,37 +28,69 @@ int main(int ac, char **av)
 			std::cout << GREEN << "SUCCESS! Correct Extension" << RESET << std::endl;
 		else
 		std::cout << RED << "ERROR: Wrong extension" << RESET << std::endl;
-		// Create container map
-		std::multimap<std::string, std::string> bitCoinLine;
-		// Check file line by line
+		// create container map
+		std::multimap<std::string, std::string> bitcoinline;
+		// check file line by line
 		std::ifstream input(av[1]);
 		for (std::string line; getline(input, line);)
 		{
 			line.erase(std::remove(line.begin(), line.end(), ' '), line.end());
 			std::cout << PURPLE << line << RESET << std::endl;
-			std::size_t pipePos = line.find('|');
-			if (pipePos != std::string::npos)
+			std::size_t pipepos = line.find('|');
+			if (pipepos != std::string::npos)
 			{
-				std::string key = line.substr(0, pipePos);
-				std::string value = line.substr(pipePos + 1);
-				if (bitCoinLine.insert(std::make_pair(key,value)) == bitCoinLine.end())
-					std::cout <<  RED << "FORMAT ERROR: MISSING A KEY OR A VALUE" << RESET << std::endl;
+				std::string key = line.substr(0, pipepos);
+				std::string value = line.substr(pipepos + 1);
+				if (bitcoinline.insert(std::make_pair(key,value)) == bitcoinline.end())
+					std::cout << RED << "format error: missing a key or a value" << RESET << std::endl;
 				else
-					std::cout << GREEN << "SUCCESSFUL PAIRING" << RESET << std::endl;
+					std::cout << GREEN << "successful pairing" << RESET << std::endl;
 			}
 			else
-			std::cout <<  RED << "FORMAT ERROR: MISSING A KEY OR A VALUE" << RESET << std::endl;
+			std::cout << RED << "format error: missing a key or a value" << RESET << std::endl;
 		}
-		std::cout << GREEN << "PRINTING MAP CONTAINER" << RESET << std::endl;
-		if (bitCoinLine.empty())
-			std::cerr << RED << "ERROR: Multimap is empty!" << RESET << std::endl;
-		for (std::multimap<std::string, std::string>::iterator iter = bitCoinLine.begin(); iter != bitCoinLine.end(); iter++)
+		std::cout << GREEN << "printing map container" << RESET << std::endl;
+		if (bitcoinline.empty())
+			std::cerr << RED << "error: multimap is empty!" << RESET << std::endl;
+		for (std::multimap<std::string, std::string>::iterator iter = bitcoinline.begin(); iter != bitcoinline.end(); iter++)
 		{
 				std::cout << iter->first << " -> " << iter->second << std::endl;
 		}
-		BitCoinExchange::checkDate(bitCoinLine);
-		BitCoinExchange::checkBtc(bitCoinLine);
+		BitCoinExchange::checkDate(bitcoinline);
+		BitCoinExchange::checkBtc(bitcoinline);
+		
 		input.close();
+		// create container map
+		std::multimap<std::string, std::string> exchangeline;
+		// check file line by line
+		std::ifstream input2("data.csv");
+		for (std::string line2; getline(input2, line2);)
+		{
+			line2.erase(std::remove(line2.begin(), line2.end(), ' '), line2.end());
+			std::cout << PURPLE << line2 << RESET << std::endl;
+			std::size_t pipepos = line2.find(',');
+			if (pipepos != std::string::npos)
+			{
+				std::string key = line2.substr(0, pipepos);
+				std::string value = line2.substr(pipepos + 1);
+				if (exchangeline.insert(std::make_pair(key,value)) == exchangeline.end())
+					std::cout << RED << "format error: missing a key or a value" << RESET << std::endl;
+				else
+					std::cout << GREEN << "successful pairing" << RESET << std::endl;
+			}
+			else
+			std::cout << RED << "format error: missing a key or a value" << RESET << std::endl;
+		}
+		std::cout << GREEN << "printing map container" << RESET << std::endl;
+		if (exchangeline.empty())
+			std::cerr << RED << "error: multimap is empty!" << RESET << std::endl;
+		for (std::multimap<std::string, std::string>::iterator iter = exchangeline.begin(); iter != exchangeline.end(); iter++)
+		{
+				std::cout << iter->first << " -> " << iter->second << std::endl;
+		}
+		BitCoinExchange::checkDate(exchangeline);
+		BitCoinExchange::checkBtc(exchangeline);
+		input2.close();
 		//MY FILE
 		//store date(key) + bitcoin(value) value in <map>
 		//check date
